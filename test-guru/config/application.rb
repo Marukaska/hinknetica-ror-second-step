77e141a4c9a81a4394238749810edebbc30987fe
Anton Dulnev <akaton@gmail.com>
2024-04-24 15:28:50 +0300

    refactor: default_locale has been changed to :ru

--- test-guru/config/application.rb
+++ test-guru/config/application.rb
@@ -23,5 +23,11 @@ module TestGuru
     #
     config.time_zone = "Europe/Moscow"
     # config.eager_load_paths << Rails.root.join("extras")
+    # Where the I18n library should search for translation files
+    I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+    # Permitted locales available for the application
+    I18n.available_locales = [:en, :ru]
+    # Set default locale to something other than :en
+    I18n.default_locale = :ru
   end
 end
