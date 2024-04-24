2e5f03b064b5a2eaa735e9a2ed0e12ecd2f32a86
Anton Dulnev <akaton@gmail.com>
2024-04-24 16:11:25 +0300

    feat: added a description of the project and the author

---
+++ test-guru/app/controllers/about/author_controller.rb
@@ -0,0 +1,4 @@
+class About::AuthorController < ApplicationController
+  def index
+  end
+end
---
+++ test-guru/test/controllers/about/author_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class About::AuthorControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
