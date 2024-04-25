b1bf2f61084f4002107f5c4eba3ca1701574dabf
Anton Dulnev <akaton@gmail.com>
2024-04-25 19:04:08 +0300

    fix: default language is set to en

--- test-guru/config/application.rb
+++ test-guru/config/application.rb
@@ -26,8 +26,8 @@ module TestGuru
     # Where the I18n library should search for translation files
     I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
     # Permitted locales available for the application
-    I18n.available_locales = [:en, :ru]
+    I18n.available_locales = [:en]
     # Set default locale to something other than :en
-    I18n.default_locale = :ru
+    I18n.default_locale = :en
   end
 end
