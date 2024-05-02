e035b964be1a098604002ea31107f81e89c24194
limepie <akaton@gmail.com>
2024-05-02 15:45:12 +0300

    feat: added a new migration for tests

---
+++ test-guru/db/migrate/20240502123735_add_tests_attribute_null_constraint.rb
@@ -0,0 +1,6 @@
+class AddTestsAttributeNullConstraint < ActiveRecord::Migration[7.1]
+  def change
+    change_column_null(:tests, :title, false)
+    change_column_null(:tests, :level, false)
+  end
+end
