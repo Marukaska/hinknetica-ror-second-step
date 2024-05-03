a285e188d108f2b4c787a4d6ef4314dcfde2045c
limepie <akaton@gmail.com>
2024-05-03 13:26:37 +0300

    Revert "feat: added a new migration for answers"
    This reverts commit 02057337c143f9c91bd718553984d1ba2ef923df.

--- test-guru/db/migrate/20240502124932_add_answers_attribute_null_constraint.rb
+++
@@ -1,6 +0,0 @@
-class AddAnswersAttributeNullConstraint < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null(:answers, :body, false)
-    change_column_null(:answers, :correct, false)
-  end
-end
