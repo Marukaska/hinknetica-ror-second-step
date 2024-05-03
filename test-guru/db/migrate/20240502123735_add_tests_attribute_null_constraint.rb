7f42115e24e95942a6dc306372fd4cb5967b5c37
limepie <akaton@gmail.com>
2024-05-03 13:26:37 +0300

    Revert "feat: added a new migration for tests"
    This reverts commit e035b964be1a098604002ea31107f81e89c24194.

--- test-guru/db/migrate/20240502123735_add_tests_attribute_null_constraint.rb
+++
@@ -1,6 +0,0 @@
-class AddTestsAttributeNullConstraint < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null(:tests, :title, false)
-    change_column_null(:tests, :level, false)
-  end
-end
