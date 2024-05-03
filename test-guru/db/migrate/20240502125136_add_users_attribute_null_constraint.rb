894c020acd5f37a9f8f65fd200030f2cb1ed4e58
limepie <akaton@gmail.com>
2024-05-03 13:26:37 +0300

    Revert "feat: added a new migration for users"
    This reverts commit 425407cd5b959067a38e86de53dc03e91cab2c9e.

--- test-guru/db/migrate/20240502125136_add_users_attribute_null_constraint.rb
+++
@@ -1,6 +0,0 @@
-class AddUsersAttributeNullConstraint < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null(:users, :name, false)
-    change_column_null(:users, :email, false)
-  end
-end
