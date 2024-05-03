9553f06e1c9910a53db70374f1b9bf9c2df7ea87
limepie <akaton@gmail.com>
2024-05-03 15:47:40 +0300

    feat: added methods for the task of the lesson

--- test-guru/app/models/test.rb
+++ test-guru/app/models/test.rb
@@ -1,3 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+  def self.sorted_titles_by_category(category_name)
+    joins(:category).where(categories: { title: category_name }).order(title: :desc).pluck(:title)
+  end
 end
