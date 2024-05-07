252d5fc136d1f0dd86e931584b701f0057f9d208
limepie <akaton@gmail.com>
2024-05-07 22:31:36 +0300

    fix: fixed name of the association

--- test-guru/app/models/user.rb
+++ test-guru/app/models/user.rb
@@ -1,13 +1,9 @@
 class User < ApplicationRecord
   has_many :status_tests
-  has_many :tests, foreign_key: :author_id
+  has_many :author_tests, class_name: 'Test', foreign_key: :author_id
   has_many :tests, through: :status_tests
 
   def tests_by_level(level)
     tests.where(level: level)
   end
-
-  # def tests_by_level(level)
-  #   Test.joins("JOIN status_tests ON tests.id = status_tests.test_id").where("status_tests.user_id = ? AND tests.level = ?", id, level)
-  # end
 end
