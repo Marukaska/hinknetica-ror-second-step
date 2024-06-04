38fbfb5561a8da2b638b0fe8f3498cebab918552
limepie <akaton@gmail.com>
2024-06-04 16:07:39 +0300

    feat: redirecting user after login

--- test-guru/app/controllers/sessions_controller.rb
+++ test-guru/app/controllers/sessions_controller.rb
@@ -9,7 +9,8 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to tests_path
+      redirect_to(session[:forwarding_url] || root_path)
+      session.delete(:forwarding_url)
     else
       flash.now[:alert] = 'Are you a Guru? Verify your Email and Password please'
       render :new
